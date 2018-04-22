526588d25a460c87acbc378fadfa425ac896530d
Glitch (voting-app-feddle) <none>
2018-04-22 13:49:17 +0000

    Fixes/private mode/cookie tracking

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const bodyParser = require("body-parser");
 const crypto = require("crypto");
+const cookieParser = require("cookie-parser");
 
 const authRoutes = require("./routes/auth-routes");
 const passportSetup = require("./config/passport-setup");
@@ -19,6 +20,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(helmet());
 app.use(express.static("public"))
+app.use(cookieParser());
 
 
 nunjucks.configure("views", {
@@ -43,7 +45,7 @@ app.use("/auth", authRoutes);
 
 //route for homepage
 app.get("/", (req, res, next) => {
-  Poll.find({}, (err, polls) => {    
+  Poll.find({private: false}, (err, polls) => {    
     res.render("index.html", {polls: polls, user: req.user});
   });
 });
@@ -61,13 +63,15 @@ app.post("/new", urlencodedParser, (req, res, next) => {
     let title = req.body.pollTitle;
     let options = req.body.options.split(/\r\n|\r|\n/g);
     let optionsObj = {};      
-    let link = crypto.randomBytes(3).toString('hex');      
+    let link = crypto.randomBytes(3).toString('hex');  
+    let isPrivate = req.body.checkBoxPrivate ? true : false;
     for(let option of options) optionsObj[option] = 0;    
     if(req.user) User.updateOne({_id: req.user.id}, {$push: {polls: link}}).then((err, success) => console.log(err));
     new Poll({
       title: title,
       options: optionsObj,
-      link: link
+      link: link,
+      private: isPrivate
     }).save().then(res.redirect("/poll/"+link));     
   }  
 });
@@ -79,6 +83,7 @@ app.get("/poll/:link", (req, res, next) => {
   if(req.user) bool = req.user.polls.includes(req.params.link)
   Poll.findOne({link: req.params.link}).then((poll) => {
     if(!poll) next();
+    if(req.cookies.polls && req.cookies.polls.includes(req.params.link)) res.render("viewPoll.html", {poll: poll, user: req.user, isOwner: bool, hasVoted: true});
     else res.render("viewPoll.html", {poll: poll, user: req.user, isOwner: bool});
   })
 });
@@ -94,9 +99,15 @@ app.get("/poll/:link/delete", urlencodedParser, (req, res, next) => {
 //route for vote posting
 app.post("/poll/:link", urlencodedParser, (req, res, next) => {
   if (!req.body) return res.sendStatus(400);     
+  if(req.cookies.polls && req.cookies.polls.includes(req.params.link)) res.redirect("/poll/"+req.params.link);
     else {                  
       let vote = req.body.customOption ? req.body.customOption : req.body.vote;      
-      Poll.updateOne({"link": req.params.link}, {$inc: {["options."+vote]: +1}}).then(res.redirect("/poll/"+req.params.link));
+      Poll.updateOne({"link": req.params.link}, {$inc: {["options."+vote]: +1}}).then(() => {        
+        let p = req.cookies.polls ? req.cookies.polls + "," + req.params.link : req.params.link;          
+        res.cookie("polls", p, { expires: new Date("2032-12-17T03:24:00"), httpOnly: true });      
+        res.redirect("/poll/"+req.params.link);
+      });
+      
     }
 });
 
